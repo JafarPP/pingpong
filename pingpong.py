5a2abb0f77beacf806dbac495ee43f4b7792d937
JafarPP <ahmedovdzafar3@gmail.com>
2024-09-02 19:17:10 +0300

    Add files via upload
    v 2.0

--- pingpong.py
+++ pingpong.py
@@ -15,6 +15,8 @@ mixer.init()
 mixer.music.load("звуки дождя.ogg")
 mixer.music.play()
 
+bal = 0
+ochko = 0
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_weight, player_height, player_speed):
@@ -46,9 +48,13 @@ class Pikachu(GameSprite):
         if keys[K_s] and self.rect.y < 445:
             self.rect.y += 10
 
+font.init()
+font3 = font.Font(None, 35)
+schet1 = font3.render(str(bal) + str("Отбил пикачу"), True, (0, 0, 255))
+schet2 = font3.render(str(ochko) + str("Отбил доги"), True, (0, 0, 255))
 
-player1 = Dog("доги.png", 605, 250, 50, 50, 5)
-player2 = Pikachu("пикачу.png", 45, 250, 50, 50, 5)
+player2 = Dog("доги.png", 605, 250, 50, 50, 5)
+player1 = Pikachu("пикачу.png", 45, 250, 50, 50, 5)
 mach = GameSprite("Мячик.png", 350, 250, 50, 50, 5)
 
 speed_x = 3
@@ -62,23 +68,27 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    if not finish:
+    if finish != True:
         window.blit(background, (0, 0))
         mach.rect.x += speed_x
         mach.rect.y += speed_y
-        if mach.rect.y >= 450:
-            mach.rect.x += speed_x
-            mach.rect.y -= speed_y
-        if mach.rect.y <= 0:
-            mach.rect.x -= speed_x
-            mach.rect.y += speed_y
+        if mach.rect.y > main_height-50 or mach.rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(player1, mach):
+            speed_x *= -1
+            bal += 1
+
+        if sprite.collide_rect(player2, mach):
+            speed_x *= -1
+            ochko += 1
 
         player1.reset()
         player2.reset()
         mach.reset()
 
-        player1.dog_update()
-        player2.pikachu_update()
+        player2.dog_update()
+        player1.pikachu_update()
 
         if mach.rect.x <= 0:
             finish = True
@@ -86,12 +96,18 @@ while game:
             font1 = font.Font(None, 70)
             loser1 = font1.render("Player_1  is lose!", True, (0, 0, 255))
             window.blit(loser1, (200, 200))
+            
         if mach.rect.x >= 650:
             finish = True
             font.init()
             font2 = font.Font(None, 70)
             loser2 = font2.render("Player_2 is lose!", True, (0, 0, 255))
             window.blit(loser2, (200, 200))
+
+        schet1 = font3.render(str(bal) + str(" отбил пикачу"), True, (0, 0, 255))
+        schet2 = font3.render(str(ochko) + str(" отбил доги"), True, (0, 0, 255))
+        window.blit(schet1, (45, 45))
+        window.blit(schet2, (45, 75))
 
         display.update()
         clock.tick(60)
